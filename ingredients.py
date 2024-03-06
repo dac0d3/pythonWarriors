55821e3d9968697a661a166a6f833431c242ff46
Diego Carbajal <diego.carbajal.482@my.csun.edu>
2024-03-05 19:23:40 -0800

    annotated

--- ingredients.py
+++ ingredients.py
@@ -2,23 +2,23 @@
 import openpyxl
 from openpyxl import Workbook, load_workbook
 
-book = load_workbook('pythonParlorInventory.xlsx')          # excel sheet
+book = load_workbook('pythonParlorInventory.xlsx')          # excel sheet for inventory
 sheet = book.active
 
-def cheesePizza(x):
-    dough = sheet['D2'].value
+def cheesePizza(x):                     
+    dough = sheet['D2'].value   # updates dough
     dough = int(dough) - x
     sheet['D2'].value = dough
     
-    cheese = sheet['B2'].value
+    cheese = sheet['B2'].value  # updates cheese
     cheese = int(cheese) - x
     sheet['B2'].value = cheese
     
-    sauce = sheet['C2'].value
+    sauce = sheet['C2'].value   # updates sauce
     sauce = int(sauce) - x
     sheet['C2'].value = sauce
     
-    book.save('pythonParlorInventory.xlsx')
+    book.save('pythonParlorInventory.xlsx')     # saves changes in excel sheet 
     
     return True 
     
