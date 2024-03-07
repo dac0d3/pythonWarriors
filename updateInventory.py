97bbb41ba0d97d35df168cd74c99be6599ae7f51
Diego Carbajal <diego.carbajal.482@my.csun.edu>
2024-03-07 15:03:39 -0800

    added roni pizza

--- updateInventory.py
+++ updateInventory.py
@@ -23,6 +23,29 @@ def cheesePizza(x):
     book.save('pythonParlorInventory.xlsx')     # saves changes in excel sheet 
     
     return True 
+
+def pepperonniPizza(x):                           
+    dough = sheet['D2'].value   # updates dough
+    dough = int(dough) - x
+    sheet['D2'].value = dough
+    
+    cheese = sheet['B2'].value  # updates cheese
+    cheese = int(cheese) - x
+    sheet['B2'].value = cheese
+    
+    sauce = sheet['C2'].value   # updates sauce
+    sauce = int(sauce) - x
+    sheet['C2'].value = sauce
+    
+    pep = sheet['A2'].value         # updates pepperonni
+    pep = int(pep) - x
+    sheet['A2'].value = pep
+    
+    book.save('pythonParlorInventory.xlsx')     # saves changes in excel sheet 
+    
+    return True 
+
+
     
 
     
