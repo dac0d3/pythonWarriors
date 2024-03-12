a0cc39fae5666604e7f509338d00d6e3814dbb4e
Diego Carbajal <diego.carbajal.482@my.csun.edu>
2024-03-12 14:28:46 -0700

    revenue

--- revenue.py
+++ revenue.py
@@ -1,2 +1,12 @@
 # calculates tax from total and sends to receipt file
 # adds total + tax to revenue after order is confirmed 
+import math
+
+
+def calculateTax (price):                  
+    total = price + (price * .0725)             # calculates tax 
+    math.trunc(total)                           # truncates decimals past 100s
+    return total
+
+def addToRevenue(total):
+    pass
