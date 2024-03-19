7cb0de3431f1f255a9356a71cdf878b669efa3db
Diego Carbajal <diego.carbajal.482@my.csun.edu>
2024-03-19 16:15:23 -0700

    order

--- confirmOrder.py
+++
@@ -1,4 +0,0 @@
-# display receipt from receipt file 
-# confirm button = (sends order to chef) && (updates inventory on excel)
-# starts a new customer transaction in customer file
-
--- order.py
+++ order.py
@@ -3,6 +3,10 @@
 # get total from dictionary and sends it to revenue file
 # sends customer to confirmOrder file
 
+# display receipt from receipt file 
+# confirm button = (sends order to chef) && (updates inventory on excel)
+# starts a new customer transaction in customer file
+
 menu = ['CHEESE PIZZA','PEPPERONI PIZZA',]
 menu_prices = {'CHEESE PIZZA':15,'PEPPERONNI PIZZA':17} # needs full menu and prices 
 
