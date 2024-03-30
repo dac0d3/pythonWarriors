9d43f88e8f714a642f730388687c41731edbdb47
Diego Carbajal <diego.carbajal.482@my.csun.edu>
2024-03-30 15:33:36 -0700

    need to make main window run everything through input

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from updateInventory import *
+from tkinter import *
 
 # make it a loop 
+
+window = Tk()
+window.title('Main')
+window.geometry('1440x900')
+
 
 import customer 
 import order 
 import checkout
 
+   
+
 
 
 #restockInventory()
+
+window.mainloop()
