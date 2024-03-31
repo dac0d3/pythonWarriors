f7bef80c31a9dc375769707ff661b88f913c8695
Diego Carbajal <diego.carbajal.482@my.csun.edu>
2024-03-31 16:38:48 -0700

    retrieved pizzas for first order from excel for chef

--- chef.py
+++ chef.py
@@ -3,22 +3,41 @@
 
 
 from tkinter import *
+import openpyxl
+from openpyxl import Workbook, load_workbook
 
+book = load_workbook('customerTransactions.xlsx')
+sheet = book.active
 
+row = 2
+cusID = row
 
-window = Tk()
+    
+numCP = sheet['D'+str(row)].value
+numPP = sheet['E'+str(row)].value
+numHP = sheet['F'+str(row)].value
+numMP = sheet['G'+str(row)].value
 
+totalPizzas = numCP + numPP + numHP + numMP
 
+def closeWindow():
+    window.destroy()
+    
+    
+if totalPizzas == 0:
+    
+    window = Tk()
+    window.title('Chef Screen')
 
+    label = Label(window,text = 'No pizzas to cook',bg='white',fg = 'black')
+    label.pack()
+    
+    button = Button(window,text = 'Submit',command = closeWindow)
+    button.pack()
+    
+    window.mainloop()
 
-
-
-
-
-
-
-
-
-
-
-window.mainloop()
+else:
+    print("Total number of pizzas to cook: "+str(totalPizzas))
+    
+    
