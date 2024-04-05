5e8cf702bcdccc2289b8d79b0d48a03a0f94ed06
Diego Carbajal <diego.carbajal.482@my.csun.edu>
2024-04-04 17:23:46 -0700

    made customer class

--- customer.py
+++ customer.py
@@ -1,80 +1,86 @@
 #name   #email   #confirm button takes to order file
 
 #doc string documentation ?
+from tkinter import *
+from tkinter.tix import LabelEntry
 import openpyxl
 from openpyxl import Workbook, load_workbook
-from tkinter import *
 
-
+#from main import Main,cusID
+    
 
 book = load_workbook('customerTransactions.xlsx')
 sheet = book.active
 
 
-cusID = sheet['A2'].value
-cusID = int(cusID)
-print('Customer ID: '+str(cusID))
-
-#name = 'customer'
-#email = 'email@abc.com'
-
 window = Tk()
 window.geometry('1440x900')
 window.title('Customer Registration')
-
-
-# gets values for name and email from customer when confirm button is clicked 
-def getVals():
-    
-    nameVal = nameEntry.get()
-    emailVal = emailEntry.get()
-    nameEntry.config(state = DISABLED)
-    emailEntry.config(state = DISABLED)
-    
-    sheet['B'+str(cusID)].value = nameVal               # stores name in excel 
-    sheet['C'+str(cusID)].value = emailVal              # stores email in excel
-    book.save('customerTransactions.xlsx')
-    
-    #print(nameVal,emailVal)
-    window.destroy()            #close window here 
-    #import order #  <-- call class for order here
-    
-    
     
 
-#Heading 
-Label(window,text = "Customer Registration",font = 'ar 20 bold').grid(row = 0,column = 3)
+cusID = sheet['A2'].value
+cusID = int(cusID)
+print('Customer ID: '+str(cusID))
+    
+class Customer:
+
+    def __init__(self,window):
+        
+        nameVal = StringVar
+        emailVal = StringVar
+        
+        #Heading 
+        Label(window,text = "Customer Registration",font = 'ar 20 bold').grid(row = 0,column = 3)
+        
+        
+        #Field Name
+        self.name = Label(window,text = "Name")
+        self.email = Label(window,text = "Email")
+        
+        self.name.grid(row = 1,column = 2)
+        self.email.grid(row = 2,column = 2)
+        
+        
+        #Creating Entry Field
+        self.nameEntry = Entry(window,textvariable=nameVal)
+        self.emailEntry = Entry(window,textvariable=emailVal)
+    
+    
+        #Packing Entry Fields
+        self.nameEntry.grid(row = 1,column = 3)
+        self.emailEntry.grid(row = 2,column = 3)
+        
+        #Submit Button
+        self.checkoutButton = Button(window,text = 'Submit',command = self.getVals)
+        self.checkoutButton.grid(row = 7,column = 3)
+        
+        
+        
+        
+    
+    # gets values for name and email from customer when confirm button is clicked 
+    def getVals(self):
+        
+        self.nameVal = self.nameEntry.get()     #nameEntry
+        self.emailVal = self.emailEntry.get()
+        self.nameEntry.config(state = DISABLED)
+        self.emailEntry.config(state = DISABLED)
+    
+        sheet['B'+str(cusID)].value = self.nameVal               # stores name in excel 
+        sheet['C'+str(cusID)].value = self.emailVal              # stores email in excel
+        book.save('customerTransactions.xlsx')
+    
+        #print(nameVal,emailVal)
+        window.destroy()            #close window here 
+        #import order #  <-- call class for order here
+    
 
 
-#Field Name
-name = Label(window,text = "Name")
-email = Label(window,text = "Email")
+    
+    
+    
+customer = Customer(window) 
 
-
-#Packing Fields
-name.grid(row = 1,column = 2)
-email.grid(row = 2,column = 2)
-
-
-#Var for storing data
-nameVal = StringVar
-emailVal = StringVar
-checkVar = IntVar
-
-
-#Creating Entry Field
-nameEntry = Entry(window,textvariable=nameVal)
-emailEntry = Entry(window,textvariable=emailVal)
-
-
-#Packing Entry Fields
-nameEntry.grid(row = 1,column = 3)
-emailEntry.grid(row = 2,column = 3)
-
-
-#Submit Button
-Button(window,text = 'Submit',command = getVals).grid(row = 7,column = 3)
-
-
+  
 window.mainloop()
 
--- main.py
+++ main.py
@@ -2,27 +2,34 @@ import openpyxl
 from openpyxl import Workbook, load_workbook
 from tkinter import *
 
+book = load_workbook('customerTransactions.xlsx')
+sheet = book.active
 
-
-#window = Tk()
-#window.title('Main')
-#window.geometry('1440x900')
-
-
-
-
-def main():
-    import customer 
-    import order 
-    import checkout
-
-main()
-
+class Main:
     
+    cusID = sheet['A2'].value
+    cusID = int(cusID)
+    #print('Customer ID: '+str(cusID))
+    
+    #window = Tk()
+    #window.title('Main')
+    #window.geometry('1440x900')
 
 
 
 
-#restockInventory()
+    def main():
+        import customer 
+        import order 
+        import checkout
 
-#window.mainloop()
+    main()
+
+
+
+
+
+
+    #restockInventory()
+
+    #window.mainloop()
