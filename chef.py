4eae93a3b5c752c498f7e9d10f4579e49ada4642
Adrian Ramos <adrianramos@Adrians-MBP-5.lan>
2024-04-21 01:30:56 -0700

    check

--- chef.py
+++ chef.py
@@ -84,38 +84,3 @@ class Chef(Toplevel):
 
 
 
-#book = load_workbook('customerTransactions.xlsx')
-#sheet = book.active
-#
-#row = 2
-#cusID = row
-#
-#    
-#numCP = sheet['D'+str(row)].value
-#numPP = sheet['E'+str(row)].value
-#numHP = sheet['F'+str(row)].value
-#numMP = sheet['G'+str(row)].value
-#
-#totalPizzas = numCP + numPP + numHP + numMP
-#
-#def closeWindow():
-#    window.destroy()
-#    
-#    
-#if totalPizzas == 0:
-#    
-#    window = Tk()
-#    window.title('Chef Screen')
-#
-#    label = Label(window,text = 'No pizzas to cook',bg='white',fg = 'black')
-#    label.pack()
-#    
-#    button = Button(window,text = 'Submit',command = closeWindow)
-#    button.pack()
-#    
-#    window.mainloop()
-#
-#else:
-#    print("Total number of pizzas to cook: "+str(totalPizzas))
-#    
-#    
