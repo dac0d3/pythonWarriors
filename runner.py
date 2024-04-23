49f16d2af29a5f92808fd733c1ab57eac770f606
Adrian Ramos <adrianramos@Adrians-MBP-5.lan>
2024-04-22 18:57:41 -0700

    runner

--- runner.py
+++ runner.py
@@ -1,7 +1,11 @@
+
+
+
 from tkinter import *
 import openpyxl
 from openpyxl import Workbook, load_workbook
 from managerSignIn import *
+import customtkinter
 
 
 
@@ -13,7 +17,7 @@ class Runner(Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
         
-        #GUI for chef window
+        #GUI for Runner window
         self.title('Orders')
         self.geometry('1400x500')
         self.config(bg = '#d9472a')
@@ -33,7 +37,7 @@ class Runner(Toplevel):
 
         for idx, row_data in enumerate(self.order_data, start=2):
             #function call
-            btn = customtkinter.CTkButton(self, text=f"Order {idx - 1}", command=lambda i=idx: self.display_row(i),bg='#d9472a',fg='black', highlightbackground='#d9472a',
+            btn = Button(self, text=f"Order {idx - 1}", command=lambda i=idx: self.display_row(i),bg='#d9472a',fg='black', highlightbackground='#d9472a',
             highlightcolor='#d9472a')
             btn.pack(side=TOP, anchor=W)
             
@@ -67,6 +71,35 @@ class Runner(Toplevel):
             self.selected_row_label.config(text="", bg='#FFC902', fg='black',font=('Arial', 18))
         else: 
             self.selected_row_label.config(text=new_text, bg='#FFC902', fg='black',font=('Arial', 18))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
